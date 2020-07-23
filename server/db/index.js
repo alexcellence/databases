721ee3a509be7fd25016ad6b32b5263c1d3553a3
Grant Taggart <granttaggartsf@gmail.com>
2020-07-23 15:26:37 -0700

    Setup create connection and messages.getAll

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,25 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'aLex&gRant',
+  database: 'chat'
+});
 
+db.connect( (err)=> {
+  if (err) {
+    throw err;
+  }
+  console.log('connected');
+});
+
+// takes as arguments queryString, queryArgs, and error first callback
+db.query(() => {
+
+})
+
+db.end(() => {
+
+})
--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,28 @@
-CREATE DATABASE chat;
+CREATE DATABASE IF NOT EXISTS chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  ID integer,
+  User varchar(30),
+  Room integer,
+  MessageText text,
+  PRIMARY KEY (ID)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  ID integer,
+  UserName varchar(30),
+  PRIMARY KEY (ID)
+);
 
-
-
+CREATE TABLE room (
+  ID integer,
+  RoomName varchar(30),
+  PRIMARY KEY (ID)
+)
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
